db7a72aa1837e2331685432cc952624e70e339dd
adjeiboyejnr <69145017+Nii-web@users.noreply.github.com>
2023-10-16 23:18:35 +0300

    Update script.js

--- psite/script.js
+++ psite/script.js
@@ -10,29 +10,30 @@
         0,0,0,0,0,0,0,0,0,0,];
 //Storing tthe array in local storage
 localStorage.setItem('array', JSON.stringify(days));*/
+
+//Retreiving the array from local storage and storing in a variable
 const days = JSON.parse(localStorage.getItem('array'));
+
 function done(id)
 {
     var el = document.getElementById(id)
-    console.log(el.style.background)
-    
-    //debugger;
+
     if(el.style.background === "rgb(207, 200, 225)")
     {
-        console.log("if " + el.style.background)
         el.style.background = "black"
         el.style.color = "white"
         days[id-1] = 1;
         localStorage.setItem('array',JSON.stringify(days));
     }
     else if(el.style.background === "black"){
-        console.log("else if " + el.style.background)
         el.style.background = "#CFC8E1"
         el.style.color = "black"
         days[id-1] = 0;
         localStorage.setItem('array',JSON.stringify(days));
     }  
 }
+
+//Creating and styling the div elements depending on their array value in local storage
 function createl(id)
 {
     const card = document.createElement("div");
@@ -40,6 +41,7 @@ function createl(id)
     document.getElementById("box").append(card);
     card.id = id;
     card.innerHTML = id
+
     if(days[id-1] === 1)
     {
         card.style.background = "black";
@@ -49,25 +51,15 @@ function createl(id)
     {
         card.style.background = "#CFC8E1";
     }
+
     card.addEventListener("click", ()=>done(id))
 }
 
+//Function called when the body is loaded to create 100 div elements
 function trackButton()
 {
     for(let i = 1; i < 101; i ++)
     {
         createl(i)
     }    
-}
-
-function readFile()
-{
-    let data = ""
-
-    var reader = new FileReader();
-
-    reader.onload = function(){
-        data = reader.result;
-    }
-    reader.readAsText(this.files[0])
 }
